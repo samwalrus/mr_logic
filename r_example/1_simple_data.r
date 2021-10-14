f1ef711be4fb917c2e4667dae9a670250b747919
Samuel Neaves <sn0550@IT017509.lan>
2021-10-14 12:51:14 +0100

    Simple R example start.

---
+++ r_example/1_simple_data.r
@@ -0,0 +1,17 @@
+########## This R code is meant to simulate data from a simple dag.
+
+### z.
+### x= f(c,z)
+### y = f2(c).
+
+### z -> x, c -> x,c -> y.
+
+### z and c are from the set {1,0}.
+
+### The domain of x is {}.
+
+### The domain of y is {}
+
+
+
+
